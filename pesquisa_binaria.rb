0f60e8d959c748053e19d6a861163e5003e6a754
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-27 15:42:32 -0300

    feat: Adiciona esqueleto do método de pesquisa binária

---
+++ pesquisa_binaria.rb
@@ -0,0 +1,13 @@
+# Pesquisa binária - mais informações no README
+
+# Regra número um da pesquisa binária é: a lista onde você vai procurar precisa estar ordenada.
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+alvo = 13
+
+# Marcadores de inicio e fim
+marcador_inicio = 0
+marcador_fim = lista.last  # O .last retorna o último item da lista
+
+def pesquisa_binaria
+    
+end
