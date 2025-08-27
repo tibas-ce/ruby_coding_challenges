f60901e2f51542015bec20fb10f4202214325174
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-27 17:14:58 -0300

    Adiciona lógica de cálculo do meio e a comparação central do algoritmo

--- pesquisa_binaria.rb
+++ pesquisa_binaria.rb
@@ -4,10 +4,23 @@
 lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 alvo = 13
 
-# Marcadores de inicio e fim
-marcador_inicio = 0
-marcador_fim = lista.last  # O .last retorna o último item da lista
 
-def pesquisa_binaria
-    
+def pesquisa_binaria (array, target)
+    # Marcadores de inicio e fim
+    # O .first retorna o primeiro (obs. poderiamos colocar o 0, pois o índice sempre inicia em 0)
+    marcador_inicio = array.first
+    # o .length é um método que retorna o tamanho (quantidade de elementos) de uma coleção como Array, String, Hash etc.
+    marcador_fim = array.length - 1
+    while marcador_inicio <= marcador_fim
+        meio = (marcador_inicio + marcador_fim) / 2
+        if meio == target
+            return meio            
+        elsif meio < target
+            marcador_inicio = meio + 1
+        elsif meio > target
+            marcador_fim = meio - 1
+        end
+    end
 end
+
+puts(pesquisa_binaria(lista, alvo))
