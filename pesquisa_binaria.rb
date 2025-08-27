f285b21852f6878d79688e3f1c46cba5f49caa04
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-08-27 18:08:22 -0300

    fix: correção da lógica central. Adicionando comentários

--- pesquisa_binaria.rb
+++ pesquisa_binaria.rb
@@ -8,22 +8,25 @@ alvo = 13
 
 def pesquisa_binaria (array, target)
     # Marcadores de inicio e fim
-    # O .first retorna o primeiro (obs. poderiamos colocar o 0, pois o índice sempre inicia em 0)
-    marcador_inicio = array.first
+    # A pesquisa binária opera com os índices, não com os valores dos elementos
+    marcador_inicio = 0
     # o .length é um método que retorna o tamanho (quantidade de elementos) de uma coleção como Array, String, Hash etc.
     marcador_fim = array.length - 1
+    # Continua a busca enquanto o marcador inicio não ultrapassa o do fim
     while marcador_inicio <= marcador_fim
+        #A divisão por 2 (/) em Ruby, com números inteiros, já arredonda para baixo, o que funciona perfeitamente para encontrar o índice central.
         meio = (marcador_inicio + marcador_fim) / 2
-        if meio == target
-            # O .index() procura onde um valor aparece e retorna a posição (ou nil se não existir).
-            meio = array.index(meio)
+        # Variável para adicionar o valor do índice ao meio
+        valor_ind_meio = array[meio]
+        if valor_ind_meio == target
             return meio            
-        elsif meio < target
+        elsif valor_ind_meio < target
             marcador_inicio = meio + 1
-        elsif meio > target
+        else # meio > target
             marcador_fim = meio - 1
         end
     end
+    # Se o loop terminar sem encontrar o alvo, retorna nil para indicar falha. (OBS. outra opção comum é retornar -1)
     return nil
 end
 
