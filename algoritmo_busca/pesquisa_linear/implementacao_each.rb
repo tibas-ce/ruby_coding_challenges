ec1871f79c68fced3a10275cdab8beb7d3690e13
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-09-02 19:39:45 -0300

    test: adicionar testes unitários para busca linear
    Inclui testes para as duas implementações:
    - busca_linear (.each)
    - busca_linear_index (.each_with_index)
    
    Cenários testados:
    - elemento no início
    - elemento no meio
    - elemento no fim
    - elemento inexistente

--- algoritmo_busca/pesquisa_linear/implementacao_each.rb
+++ algoritmo_busca/pesquisa_linear/implementacao_each.rb
@@ -1,12 +1,5 @@
 # Linear search (busca linear) - Mais informações readme
 
-# arrays para cada caso
-array = [10, 25, 43, 33, 190, 5]
-array_primeiro = [43, 25, 343, 33, 190, 5]
-array_ultimo = [10, 25, 23, 33, 190, 43]
-array_sem = [10, 25, 56, 33, 190, 5]
-alvo = 43
-
 # busca_linear com .each
 def busca_linear (array, alvo)
   indice = 0
@@ -22,11 +15,3 @@ def busca_linear (array, alvo)
   # Retorna -1 caso o alvo não seja encontrado no array
   return -1
 end
-
-# teste
-resposta = busca_linear(array, alvo)
-resposta2 = busca_linear(array_primeiro, alvo)
-resposta3 = busca_linear(array_ultimo, alvo)
-resposta4 = busca_linear(array_sem, alvo)
-
-puts(resposta, resposta2, resposta3, resposta4)
