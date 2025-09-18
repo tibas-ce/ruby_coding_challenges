f239bfac2a21f8d43acd2fac878191b2fbc7e982
tiberio_s_ferreira <tiberio.ferreiracs@gmail.com>
2025-09-18 08:25:39 -0300

    feat: adiciona testes para função find_future_naive

--- algorithms/search/find_future/find_future_test.rb
+++ algorithms/search/find_future/find_future_test.rb
@@ -12,7 +12,7 @@ class TestFindFuture < Minitest::Test
     # 32/13/2020 não existe
     @date_not_exist = ["32/13/2020", "15/08/2500"]
     # String vazia
-    @invalid_date = ["", "32/13/2020", "15/08/2500"]
+    @invalid_date = ["", "datas", "32/13/2020", "15/08/2500"]
   end
 
   # Testes para find_future_efficient
@@ -29,13 +29,33 @@ class TestFindFuture < Minitest::Test
 
   # Teste com uma data inválida no array
   def test_invalid_date_in_array_efficient
-    result = find_future_efficient(@date_not_exist, "1/1/2000")
-    # A única válida é 15/08/2500, então ela deve ser retornada
-    assert_equal Date.new(2500,8,15), result
+    assert_equal Date.new(2500,8,15), find_future_efficient(@date_not_exist, "1/1/2000")
   end
   
   # Quando a consult_date é exatamente igual a uma das datas
   def test_consult_date_equal_efficient
     assert_equal Date.new(1233,4,22), find_future_efficient(@dates, "22/4/1233")
+  end
+
+  # Testes para find_future_naive
+  # Caso normal: existe uma dat futura
+  def test_future_exists_naive
+    assert_equal Date.new(1233,4,22), find_future_naive(@dates, "1/1/1000")
+  end
+
+  # Nenhuma data futura existe
+  def test_no_future_exists_naive
+    #result = 
+    assert_equal(-1, find_future_naive(@dates, "1/1/70000")) 
+  end
+
+  # Teste com strings inválida ou vazias
+  def test_invalid_date_array_naive
+    assert_equal Date.new(2500,8,15), find_future_naive(@date_not_exist, "1/1/2000")
+  end
+
+  # Quando a cunsult_date é exatamente igual a uma data
+  def test_consult_date_equal_naive
+    assert_equal Date.new(1233,4,22), find_future_naive(@dates, "22/4/1233")
   end
 end
